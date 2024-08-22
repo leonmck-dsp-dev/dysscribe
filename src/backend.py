f3767516a86cb2b313195c255cc601fdb7b7d9ce
Leon mckenzie <djrowanater@gmail.com>
2024-08-22 17:59:47 +0100

    Create backend.py

---
+++ src/backend.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sounddevice as sd
+import csv
+import keyboard
+
+
+
+
+
+
+def record_audio(file_name, sr=44100, channels=2):
+    rec = []
+    is_recording = false
+    def callback(indata, frames, time, status):
+        nonlocal rec, is_recording
+        if is_recording:
+            rec.append(indata.copy())
+    with sd.InputStream(callback=callback, samplerate=sr, channels=channels):
+        print('Press space to start recording')
+        keyboard.wait('space')
+        is_recording = True
+        print('Recording... Press space to stop')
+        keyboard.wait('space')
+        is_recording = False
+        print('Stopped recording now saving...')
+
+        if rec:
+        recorded_array = np.concatenate(rec, axis=0)
+        sf.write(filename, recorded_array, samplerate)
+        print(f"Saved to {filename}")
+    else:
+        print("No audio recorded.")
